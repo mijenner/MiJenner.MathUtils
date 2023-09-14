c1ee9e4544d1421a735a9bd767e6d95fa7fbb390
Michael Jenner <mijenner@gmail.com>
2023-09-14 23:49:21 +0200

    Add project files.

---
+++ MathUtils/MathUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MiJenner
+{
+    public static class MathUtils
+    {
+        /// <summary>
+        /// RoundToSignificantDigits rounds a double to 'digits' significant digits. 
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        /// Ref : https://stackoverflow.com/questions/374316/round-a-double-to-x-significant-figures/374470 
+
+        public static double RoundToSignificantDigits(double d, int digits)
+        {
+            return double.Parse(d.ToString("G" + digits));
+        }
+
+        public static double TruncateToSignificantDigits(this double d, int digits)
+        {
+            bool isNeg = false; 
+            if (d == 0)
+                return 0;
+
+            if (d < 0)
+            {
+                isNeg = true;
+                d = -d; 
+            }
+
+            double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1 - digits);
+            if (isNeg)
+            {
+                d = -d;  
+            }
+
+            return scale * Math.Truncate(d / scale);
+        }
+    }
+}
---
+++ UsageExamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MiJenner; 
+
+namespace UsageExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.Write((a = 345.67) + " converts to ");
+            Console.Write(MathUtils.RoundToSignificantDigits(a, 3));
+            Console.WriteLine(" with 3 significant figures");
+            Console.WriteLine("and");
+            Console.Write(a + " truncates to ");
+            Console.Write(MathUtils.TruncateToSignificantDigits(a, 3));
+            Console.WriteLine(" with 3 significant figures");
+
+            Console.WriteLine("---------------");
+
+            Console.Write((a = 345.67) + " converts to ");
+            Console.Write(MathUtils.RoundToSignificantDigits(a, 7));
+            Console.WriteLine(" with 7 significant figures");
+            Console.WriteLine("and");
+            Console.Write(a + " truncates to ");
+            Console.Write(MathUtils.TruncateToSignificantDigits(a, 7));
+            Console.WriteLine(" with 7 significant figures");
+        }
+    }
+}
